709c7500b10f9aa58584404070674b68b4414f59
AthreyaRaghavan2 <144204933+AthreyaRaghavan2@users.noreply.github.com>
2023-11-09 09:57:56 -0800

    Add files via upload
    Hangman?

---
+++ Hangman/src/Hangman.java
@@ -0,0 +1,27 @@
+
+public class Hangman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void hangedMan(int stage) {
+		if (stage == 0) {
+			
+		}
+	}
+	public static void gallows() {
+		
+		
+		
+	}
+	public static void lineHor(int amount) {
+		for(int i = 1; i <= amount; i++) {
+			System.out.print("-");
+		}
+	}
+	public static void lineVer(int amount) {
+		
+	}
+
+}
